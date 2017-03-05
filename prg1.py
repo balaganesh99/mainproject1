cb5338265cf28b91a0c09fb66d8d51835f0eb53b
bala <balaganesh.balaganesh@gmail.com>
2017-03-05 02:24:23 -0600

    false commit

---
+++ prg1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 05 01:17:48 2017
+
+@author: Balaganesh
+"""
+
+import pandas as pd
+import numpy as np
---
+++ prg2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 05 01:25:46 2017
+
+@author: Balaganesh
+"""
+
+ a= 10
+ b = 20
+ c = a + b 
+ c
+ 
+ d = c /a
